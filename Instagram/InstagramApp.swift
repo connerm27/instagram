3dc05d4f62d3e1a19c077a7ccde3083013a16dac
Conner Montgomery <cmontgomery2799@gmail.com>
2021-02-15 21:58:32 -0500

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Conner Montgomery on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
